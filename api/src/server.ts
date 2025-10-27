5f89a22cd42919d37129702d0c0547c2292098e4
andregm <andre@appdev.com.br>
2025-10-27 11:32:35 -0300

    feat: create structure with docker, drizzle and fastify

---
+++ api/src/server.ts
@@ -0,0 +1,58 @@
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+} from 'fastify-type-provider-zod'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from './routes/list-webhooks'
+import { env } from './env'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // credentials: true,
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API for inspecting webhooks',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  // options
+  routePrefix: '/docs',
+})
+
+app.get('/', async () => {
+  return { path: '/' }
+})
+
+app.get('/health', async () => {
+  return { status: 'ok' }
+})
+
+app.register(listWebhooks)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at http://localhost:${env.PORT}`)
+  console.log(`API docs available at http://localhost:${env.PORT}/docs`)
+})
