1d2d697022ff5049f339c1edb04fd8d5b237cafb
andregm <andre@appdev.com.br>
2025-10-29 10:18:14 -0300

    feat: create structure of frontend

---
+++ web/src/components/section-data-table.tsx
@@ -0,0 +1,32 @@
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface SectionDataTableProps extends ComponentProps<"div"> {
+	data: Array<{ key: string; value: string }>;
+}
+export function SectionDataTable({
+	className,
+	data,
+	...props
+}: SectionDataTableProps) {
+	return (
+		<div
+			className={twMerge(
+				"rounded-lg bg-zinc-800/50 border border-zinc-700 overflow-hidden",
+				className,
+			)}
+			{...props}
+		>
+			<table className="w-full">
+				{data.map(({ key, value }) => (
+					<tr key={key} className="border-b border-zinc-700 last:border-0">
+						<td className="p-3 text-sm font-medium text-zinc-400 bg-zinc-800/50 border-r border-zinc-700">
+							{key}
+						</td>
+						<td className="p-3 text-sm font-mono text-zinc-300">{value}</td>
+					</tr>
+				))}
+			</table>
+		</div>
+	);
+}
---
+++ web/src/components/webhooks-detail-header.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "./ui/badge";
+export function WebhooksDetailHeader() {
+	return (
+		<div className="space-y-4 border-b border-zinc-700 p-6">
+			<div className="flex items-center gap-3">
+				<Badge>POST</Badge>
+				<span className="text-lg font-medium text-zinc-300">/video/status</span>
+			</div>
+			<div className="flex items-center gap-2">
+				<div className="flex items-center gap-1 text-sm text-zinc-400">
+					<span>From IP</span>
+					<span className="underline underline-offset-4">192.168.0.1</span>
+				</div>
+				<span className="w-px h-4 bg-zinc-700"></span>
+				<div className="flex items-center gap-1 text-sm text-zinc-400">
+					<span>at</span>
+					<span>April 18th, 1pm</span>
+				</div>
+			</div>
+		</div>
+	);
+}
